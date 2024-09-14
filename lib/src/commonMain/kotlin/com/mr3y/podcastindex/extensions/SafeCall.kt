01f9c8d4a0158feac8abbbdfb69a324932c7288e
MR3Y <abdonasr379@gmail.com>
2024-09-15 01:38:34 +0300

    Simplify withErrorHandling

--- lib/src/commonMain/kotlin/com/mr3y/podcastindex/extensions/SafeCall.kt
+++ lib/src/commonMain/kotlin/com/mr3y/podcastindex/extensions/SafeCall.kt
@@ -1,7 +1,6 @@
 package com.mr3y.podcastindex.extensions
 
 import com.mr3y.podcastindex.model.BadRequestException
-import com.mr3y.podcastindex.model.InternalException
 import com.mr3y.podcastindex.model.UnknownException
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
@@ -11,19 +10,14 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
 internal suspend inline fun <reified T> withErrorHandling(block: () -> HttpResponse): T {
-    return try {
-        val response = block()
-
-        when (response.status) {
-            HttpStatusCode.OK -> response.body()
-            HttpStatusCode.BadRequest -> {
-                val description = response.body<JsonObject>()["description"]!!.jsonPrimitive.content
-                throw BadRequestException(description)
-            }
-            HttpStatusCode.Unauthorized -> throw IllegalStateException("You're Unauthenticated! Make sure to enter your authentication credentials correctly")
-            else -> throw UnknownException(response.bodyAsText())
+    val response = block()
+    return when (response.status) {
+        HttpStatusCode.OK -> response.body()
+        HttpStatusCode.BadRequest -> {
+            val description = response.body<JsonObject>()["description"]!!.jsonPrimitive.content
+            throw BadRequestException(description)
         }
-    } catch (ex: Exception) {
-        throw InternalException(ex.message, ex.cause)
+        HttpStatusCode.Unauthorized -> throw IllegalStateException("You're Unauthenticated! Make sure to enter your authentication credentials correctly")
+        else -> throw UnknownException(response.bodyAsText())
     }
 }
--- lib/src/commonMain/kotlin/com/mr3y/podcastindex/model/Exception.kt
+++ lib/src/commonMain/kotlin/com/mr3y/podcastindex/model/Exception.kt
@@ -2,6 +2,4 @@ package com.mr3y.podcastindex.model
 
 class BadRequestException(override val message: String) : Exception(message)
 
-class InternalException(override val message: String?, override val cause: Throwable?) : Exception(message, cause)
-
 class UnknownException(override val message: String) : Exception(message)
