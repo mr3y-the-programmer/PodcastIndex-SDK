4697e1e510b99d611011f887ab0e2488950596d8
MR3Y <abdonasr379@gmail.com>
2024-10-15 02:48:44 +0300

    Elaborate on OptIn message

--- podcastindex-sdk-base/src/commonMain/kotlin/com/mr3y/podcastindex/InternalPodcastIndexApi.kt
+++ podcastindex-sdk-base/src/commonMain/kotlin/com/mr3y/podcastindex/InternalPodcastIndexApi.kt
@@ -1,7 +1,7 @@
 package com.mr3y.podcastindex
 
 @RequiresOptIn(
-    "This API is internal and is subject to be changed or removed without any prior notice or migration guide",
+    "This API is internal and isn't meant to be used in downstream projects. It's public only for technical limitations, and it is subject to be changed or removed without any prior notice or migration guide",
     level = RequiresOptIn.Level.WARNING,
 )
 @Target(
